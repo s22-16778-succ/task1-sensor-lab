a74c508290350c861529f1ec80a0a1f98615ca87
ipeon <99043860+ipeon@users.noreply.github.com>
2022-02-04 17:16:16 -0500

    Add files via upload

---
+++ distance_function_ultrasonic.ino
@@ -0,0 +1,40 @@
+
+int trig_pin = 12;
+int echo_pin = 11;
+double travel_time;
+double distance;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (trig_pin, OUTPUT);
+
+pinMode (echo_pin, INPUT);
+
+Serial.begin (9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  distance_fn();
+
+}
+
+
+ double distance_fn() {
+  digitalWrite (trig_pin, LOW);
+  delay (1);
+
+  digitalWrite (trig_pin, HIGH);
+  delay(1);
+
+  digitalWrite (trig_pin, LOW);
+
+  travel_time = pulseIn (echo_pin, HIGH);
+  distance = (((travel_time / 2) / 29)/ 2.54);
+  delay (200);
+
+  Serial.print ("The distance is (inches):");
+  Serial.println (distance);
+
+ 
+}
