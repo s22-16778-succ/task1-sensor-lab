873468df02edba648315bd1349360674a718db81
sssaini <sssaini011@gmail.com>
2022-02-04 13:34:41 -0500

    new rotpot (cleaner)

--- lab1rotpot.ino
+++ lab1rotpot.ino
@@ -5,17 +5,22 @@ void setup() {
 
 const int POT_PIN = 14;
 
+void getRotPot() {
+
+  // get output num from rotpot and convert to voltage
+  double potVolt = analogRead(POT_PIN);
+  double OutVal = (potVolt/1023.0)*5.0;
+
+  // convert doubles to strings
+  String pot_out = String(potVolt,3);
+  String volt_out = String(OutVal,3);
+
+  // print out values
+  Serial.println("Sensor Output + Voltage: (" + pot_out + "," + volt_out + ")");
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  double potVolt = analogRead(POT_PIN);
+  getRotPot();
 
-  String pot = String(potVolt,3);
-  
-  // print output to console
-  Serial.println("Sensor Output Value: " + pot);
-  double OutVal = (potVolt/1023.0)*5.0;
-  String volt = String(OutVal,3);
-  Serial.println("Sensor Output + Voltage: (" + pot + "," + volt + ")");
-
-  delay(500);
 }
