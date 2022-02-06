21ef080f54743faa7455cec20056ea0b4170d295
Brandon Wang <brandonwang2000@gmail.com>
2022-02-06 14:51:54 -0500

    Brandon's first integrated program

---
+++ sensor-lab/sensor-lab.ino
@@ -0,0 +1,73 @@
+// Pin assignments
+const int ECHO_PIN = 11;
+const int TRIG_PIN = 12;
+const int POT_PIN = A0;
+
+// Global state
+char last, readIn;
+
+void setup() {
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  Serial.begin (9600);
+}
+
+// Ultrasonic Distance Sensor
+void readUDS() {
+  digitalWrite(TRIG_PIN, LOW);
+  delay(1);
+  digitalWrite(TRIG_PIN, HIGH);
+  delay(1);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  double travel_time = pulseIn(ECHO_PIN, HIGH);
+  double distance = travel_time / (2 * 29); // in centimeters
+
+  Serial.print("Distance (cm): ");
+  Serial.println(distance, 3);
+  delay(200);
+}
+
+// Rotary Potentiometer
+void readPot() {
+  // get output num from rotpot and convert to voltage
+  double pot_out = analogRead(POT_PIN);
+  double volt_out = (potVolt/1023.0)*5.0;
+
+  Serial.print("Sensor Output: ");
+  Serial.print(pot_out, 3);
+  Serial.print("\tVoltage: ");
+  Serial.println(volt_out, 3);
+  delay(200);
+}
+
+// IR Proximity Sensor
+void readIR(){
+  
+}
+
+void flagSwitch(char c) {
+  switch (c) {
+    case 'u':
+      readUDS();
+      break;
+    case 'p':
+      readPot();
+      break;
+    case 'i':
+      readIR();
+      break;
+    default:
+      Serial.print(c);
+      Serial.println(" is not a valid input. Please enter 'u', 'p', or 'i'.");
+  }
+}
+
+void loop() {
+  if (Serial.available()) {
+    readIn = Serial.read();
+    if ((int) readIn == 10) // line feed
+      flagSwitch(last);
+    last = readIn;
+  }
+}
