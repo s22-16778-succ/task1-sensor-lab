ccf5337c553b9896676b8772c88ff2021209887e
DuelystRook <31782796+DuelystRook@users.noreply.github.com>
2022-02-07 15:56:28 -0500

    Update sensor-lab.ino
    added in IR Sensor method and associated global variable

--- sensor-lab/sensor-lab.ino
+++ sensor-lab/sensor-lab.ino
@@ -1,9 +1,15 @@
-
+#include<math.h>
 
 // Pin assignments
 const int ECHO_PIN = 11;
 const int TRIG_PIN = 12;
 const int POT_PIN = A0;
+const int IR_PIN = A1;
+const float a = -8169.1;
+const float b = 4789.2;
+const float c = 18.8;
+float IR_Sum = 0;
+float IR_Count = 0;
 
 // Global state
 char last, readIn;
@@ -34,7 +40,7 @@ void readUDS() {
 void readPot() {
   // get output num from rotpot and convert to voltage
   double pot_out = analogRead(POT_PIN);
-  double volt_out = (potVolt/1023.0)*5.0;
+  double volt_out = (pot_out/1023.0)*5.0;
 
   Serial.print("Sensor Output: ");
   Serial.print(pot_out, 3);
@@ -45,8 +51,29 @@ void readPot() {
 
 // IR Proximity Sensor
 void readIR(){
+  int value = analogRead(IR_PIN); //Read the distance in cm and store it
+
+  IR_Sum = IR_Sum+value;
+  IR_Count = IR_Count + 1;
   
+  if (IR_Count == 30)
+    {
+    float v = (IR_Sum/IR_Count); // average voltage over last 30 samples
+
+    if (v <= 585) // only outputs if the average is greater than the output at 10cm since the sensor is rated for 10-80cm
+      {
+      float x = sqrt(-a/(c-v) + pow(b/(2*(c-v)),2)) - b/(2*(c-v));
+       
+      Serial.println("Distance = "+String(x)+(" cm"));  //display the string e.g "Distance = 40 cm"
+
+     }
+    IR_Sum = 0;
+    IR_Count = 0;
+  }
+    
+   delay(100);//ten smaples per second
 }
+  
 
 void flagSwitch(char c) {
   switch (c) {
